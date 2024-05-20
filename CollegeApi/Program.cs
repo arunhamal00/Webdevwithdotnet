e024920bae347b5ea387773883a96e07c4ab332d
Arun Hamal <thakuriarun565@gmail.com>
2024-05-20 09:54:37 +0545

    file for webapi

--- CollegeApi/Program.cs
+++ CollegeApi/Program.cs
@@ -4,10 +4,28 @@ builder.Services.AddDbContext<CollegeDbContext>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "learning developing REST API on .NET");
 app.MapGet("/courses", (CollegeDbContext db) => db.Courses.ToList());
 app.MapGet("/courses/{id:int}", (int id, CollegeDbContext db) => db.Courses.Find(id));
 app.MapGet("/sessions", (CollegeDbContext db) => db.Sessions.ToList());
 app.MapGet("/sessions/{id:int}", (int id, CollegeDbContext db) => db.Sessions.Find(id));
+
+app.MapPost("/courses", (Course course, CollegeDbContext db) =>
+{
+    db.Courses.Add(course);
+    db.SaveChanges();
+});
+app.MapPut("/courses", (Course course, CollegeDbContext db) =>
+{
+    db.Courses.Update(course);
+    db.SaveChanges();
+});
+app.MapDelete("/courses", (Course course, CollegeDbContext db) =>
+{
+    db.Courses.Delete(course);
+    db.SaveChanges();
+});
+
+
 
 app.Run();
--- CollegeMonitor/Pages/courses/Course.cshtml.cs
+++ CollegeMonitor/Pages/courses/Course.cshtml.cs
@@ -3,9 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class CoursesModel: PageModel
 {    
     public List<Course> Courses { get; set; }
-    public void OnGet()
+    public async Task OnGet()
     {
-        CollegeDbContext db = new();
-        Courses = db.Courses.ToList();
+       // CollegeDbContext db = new();
+       // Courses = db.Courses.ToList();
+
+        //http call
+        HttpClient http = new HttpClient();
+        Courses = await http.GetFromJsonAsync<List<Course>>("http://localhost:5250/courses");
     }
 }
