ad79f866bd146820d4cb4a8de0b578671d4d00f4
Arun Hamal <thakuriarun565@gmail.com>
2024-05-20 07:12:36 +0545

    files for collegeapi

---
+++ CollegeApi/Data/CollegeDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class CollegeDbContext : DbContext
+{
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=collegeMonitor.db");
+    }
+}
