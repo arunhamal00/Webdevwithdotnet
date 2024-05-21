3b3e4a50f6e73d5bf29831cbcb0b040a284ae8d6
Arun Hamal <thakuriarun565@gmail.com>
2024-05-21 13:58:20 +0545

    files

--- CollegeApi/Program.cs
+++ CollegeApi/Program.cs
@@ -15,36 +15,4 @@ app.MapPost("/courses", (Course course, CollegeDbContext db) =>
     db.Courses.Add(course);
     db.SaveChanges();
 });
-app.MapPut("/courses", (int id, Course course, CollegeDbContext db) =>
-{
-    db.Courses.Update(course);
-    db.SaveChanges();
-});
-
-app.MapDelete("/courses", (int id, Course course, CollegeDbContext db) =>
-{
-    db.Courses.Remove(course);
-    db.SaveChanges();
-});
-
-//for Session
-app.MapPost("/sessions", (Session session, CollegeDbContext db) =>
-{
-    db.Sessions.Add(session);
-    db.SaveChanges();
-});
-
-app.MapPut("/session", (int id, Session session, CollegeDbContext db) =>
-{
-    db.Sessions.Update(session);
-    db.SaveChanges();
-});
-
-app.MapDelete("/sessions", (int id, Session session, CollegeDbContext db) =>
-{
-    db.Sessions.Remove(session);
-    db.SaveChanges();
-});
-
-
 app.Run();
