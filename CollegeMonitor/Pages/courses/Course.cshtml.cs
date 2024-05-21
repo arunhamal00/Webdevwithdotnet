3087dc17957068efa9f6abd1357209df78b0e606
Arun Hamal <thakuriarun565@gmail.com>
2024-05-21 13:58:12 +0545

    files for project

--- CollegeMonitor/Pages/courses/Course.cshtml.cs
+++ CollegeMonitor/Pages/courses/Course.cshtml.cs
@@ -1,15 +1,15 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-public class CoursesModel: PageModel
-{    
+public class CoursesModel : PageModel
+{
     public List<Course> Courses { get; set; }
     public async Task OnGet()
     {
-       // CollegeDbContext db = new();
-       // Courses = db.Courses.ToList();
+        // CollegeDbContext db = new();
+        // Courses = db.Courses.ToList();
 
         //http call
         HttpClient http = new HttpClient();
-        Courses = await http.GetFromJsonAsync<List<Course>>("http://localhost:5250/courses");
+        Courses = await http.GetFromJsonAsync<List<Course>>("http://localhost:5250/Courses");
     }
 }
